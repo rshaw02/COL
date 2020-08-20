dbd398744775da39a66b77a0ed5162a84a85d48d
rshaw02 <rshaw@student.neumont.edu>
2020-08-20 14:30:31 -0600

    All game elements copied into a seperate library; application still contains those elements, will remove with next commit.

---
+++ CardsAgainstHumanityLibrary/Models/Common/NameAttribute.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CardsAgainstHumanityLibrary
+{
+	[AttributeUsage(AttributeTargets.Field)]
+	public sealed class NameAttribute : Attribute
+	{
+		public string Name { get; }
+
+		public NameAttribute(string name)
+		{
+			Name = name;
+		}
+	}
+}
---
+++ CardsAgainstHumanityLibrary/Models/Trophies/TrophyRequirement.cs
@@ -0,0 +1,21 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CardsAgainstHumanityLibrary.Converters;
+
+namespace CardsAgainstHumanityLibrary.Trophies
+{
+	[JsonConverter(typeof(TrophyRequirementConverter))]
+	public abstract class TrophyRequirement
+	{
+		public TrophyRequirement()
+		{
+		}
+
+		public abstract bool CheckPlayer(Player player);
+	}
+}
